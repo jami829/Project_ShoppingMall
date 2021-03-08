4759c6882fa520754a9e8d951f70a32e320657b6
이재훈 <jami8291215@gmail.com>
2021-03-08 12:50:44 +0900

    :art: dark toggle using Material

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 import Header from './components/Header';
 
+import { Switch } from '@material-ui/core';
 
 
 
@@ -22,10 +23,28 @@ const theme = createMuiTheme({
 
 function App() {
 
+  // 다크모드  
+  const [isDark, setIsDark] = useState(false)
+
+  // const classes = useStyles();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsDark(event.target.checked);
+  };
+
+  console.log("isDark", isDark)
   return (
     // </ThemeProvider> 는 항상 리액트의 컨택스트 API를 의존하기 때문에 스타일링하는 컴포넌트들의 상위에 항상 위치하도록 하여야 한다.
     <ThemeProvider theme={theme}>
-      <Header />
+      <Header>
+        {/* Header 안 쪽에 children의 특수한 prop형태로 내려주게 될 것 */}
+        <Switch
+          checked={isDark}
+          onChange={handleChange}
+          name="checkedA"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+      </Header>
     </ThemeProvider>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { createStyles, makeStyles, Theme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -17,26 +19,34 @@ const useStyles = makeStyles((theme: Theme) =>
     menuButton: {
       marginRight: theme.spacing(2),
     },
+    title: {
+      flexGrow: 1,
+    },
   }),
 );
 
+interface Props {
+  children: React.ReactNode;
+}
 
+function Header({ children }: Props) {
 
-function Header() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-
       <AppBar position="static">
-        <Toolbar variant="dense">
+        <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" color="inherit">
-            Photos
-    </Typography>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          {children}
+          {/* <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
+
     </div>
   )
 }
